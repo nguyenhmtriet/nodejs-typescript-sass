80c29ef478cf2cc782cfbe351fd29eb87d09ad86
Triet Nguyen <>
2023-10-26 02:38:53 +0700

    hot reload typescript for server.ts & scripts.ts on both client & server side

---
+++ src/app/scripts.ts
@@ -0,0 +1,5 @@
+const localVideoElem = document.querySelector('#local-video');
+const remoteVideoEl = document.querySelector('#remote-video');
+
+console.log("%clocalVideoElem: %O", "background-color: green", localVideoElem);
+console.log("%cremoteVideoEl: %O", "background-color: green", remoteVideoEl);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import * as dotenv from 'dotenv';
+import path from "path";
 
 dotenv.config({ path: '.env' });
 
 const PORT = process.env.NODE_PORT;
 const app = express();
 
-app.use(express.static(__dirname));
+const rootDir = path.resolve(__dirname + "/../")
+app.use(express.static(rootDir));
 app.listen(PORT);
+
